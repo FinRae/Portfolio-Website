dffbf6b4633052b7b1996a669b2d2bd903aaa443
Coolfrae <Coolfrae@gmail.com>
2023-01-13 08:46:59 +0000

    More Console Commands

--- Console.js
+++ Console.js
@@ -25,22 +25,35 @@ function addResultOutput(output) {
 	historyContainer.append(outputLogElement)
 }
 
+function redirectDelay(page){
+	location.href = page;
+}
+
+
 consoleInput.addEventListener("keyup", e => {
 	const code = consoleInput.value.trim();
 	const codeLower = code.toLowerCase();
 	if (e.key === "Enter") {
-		
-		switch(codeLower){
-			case "aboutme":
+		if(codeLower ==="help"){
+			addResult(code,"help - Lists all valid commands")
+			addResultOutput("aboutme - Prints the about me paragraph")
+			addResultOutput("<JavaScript code> - You can run any JavaScript Code in this console, try 1+1")
+		}
+		if(codeLower === "aboutme"){
 				addResult(code,"Hey there, my name is Finlay Rae and I'm 17 years old. Im studying Mathematics, Computer Science and Physics at A level but particularly enjoy Computer Science. I code as a passion but also love to play video games, namely valorant. When I choose to code something its usually in python or C# however I know C#, Python, Lua, HTML, CSS and JS. My main interest in computer science is the field of Cyber Security, which i plan to study at university in 2023. If you wish to contact me, please do so in the contact me section of the page.")
-				break;
-			default:
-				try {
-					addResult(code, eval(code));
-				} catch (err) {
-					addResult(code,err);
-				}
-				break;
+		}
+		
+		if(codeLower.startsWith("red")){
+			if (codeLower.split(" ")[1] == "home"){
+				addResult(code,"Redirecting in 3 seconds.");
+				setTimeout(redirectDelay,3000,"home.html");
+			}
+		}
+
+		try {
+			addResult(code, eval(code));
+		} catch (err) {
+			addResult(code,err);
 		}
 		
 		
