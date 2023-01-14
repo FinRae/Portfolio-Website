1fdcb2275a98fb0931301df78b9c21a337bd82c7
Coolfrae <Coolfrae@gmail.com>
2023-01-14 02:43:03 +0100

    Console Updates

--- Console.js
+++ Console.js
@@ -33,31 +33,52 @@ function redirectDelay(page){
 consoleInput.addEventListener("keyup", e => {
 	const code = consoleInput.value.trim();
 	const codeLower = code.toLowerCase();
+	var doJs = true;
 	if (e.key === "Enter") {
 		if(codeLower ==="help"){
 			addResult(code,"help - Lists all valid commands")
 			addResultOutput("aboutme - Prints the about me paragraph")
 			addResultOutput("red <page> - Redirects you to a given page e.g. Red Home would take to the home page, Red https://www.google.co.uk/ - would take you to the google webpage")
 			addResultOutput("<JavaScript code> - You can run any JavaScript Code in this console, try 1+1")
+			doJs = false;
 		}
 		if(codeLower === "aboutme"){
-				addResult(code,"Hey there, my name is Finlay Rae and I'm 17 years old. Im studying Mathematics, Computer Science and Physics at A level but particularly enjoy Computer Science. I code as a passion but also love to play video games, namely valorant. When I choose to code something its usually in python or C# however I know C#, Python, Lua, HTML, CSS and JS. My main interest in computer science is the field of Cyber Security, which i plan to study at university in 2023. If you wish to contact me, please do so in the contact me section of the page.")
+				addResult(code,"Hey there, my name is Finlay Rae and I'm 17 years old. Im studying Mathematics, Computer Science and Physics at A level but particularly enjoy Computer Science. I code as a passion but also love to play video games, namely valorant. When I choose to code something its usually in python or C# however I know C#, Python, Lua, HTML, CSS and JS. My main interest in computer science is the field of Cyber Security, which i plan to study at university in 2023. If you wish to contact me, please do so in the contact me section of the page.");
+			doJs = false;
 		}
 		
 		if(codeLower.startsWith("red")){
+			var isUrl = true;
 			if (codeLower.split(" ")[1] == "home"){
-				addResult(code,"Redirecting in 3 seconds.");
-				setTimeout(redirectDelay,3000,"home.html");
+				addResult(code,"Redirecting to Page: Home in 3 seconds.");
+				setTimeout(redirectDelay,3000,"Home.html");
+				isUrl = false;
 			}
+			if (codeLower.split(" ")[1] == "specs"){
+				addResult(code,"Redirecting to Page: Specs in 3 seconds.");
+				setTimeout(redirectDelay,3000,"Specs.html");
+				isUrl = false;
+			}
+			if (codeLower.split(" ")[1] == "projects"){
+				addResult(code,"Redirecting to Page: Projects in 3 seconds.");
+				setTimeout(redirectDelay,3000,"Projects.html");
+				isUrl = false;
+			}
+			if(isUrl === true){
+				addResult(code,"Redirecting in 3 seconds.");
+				setTimeout(redirectDelay,3000,codeLower.split(" ")[1]);
+			}
+			doJs = false
 		}
 
-		try {
-			addResult(code, eval(code));
-		} catch (err) {
-			addResult(code,err);
+		if(doJs===true){
+			try {
+				addResult(code, eval(code));
+			} catch (err) {
+				addResult(code,err);
+			}
+		
 		}
-		
-		
 		
 		
 		
